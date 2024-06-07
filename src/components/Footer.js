af2e480508cca10d38379a1e99453567f5e67300
Jeevan-30 <162351733+Jeevan-30@users.noreply.github.com>
2024-06-08 01:45:52 +0530

    Add files via upload

---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <footer id="footer">
+                <div className="container">
+                    <a
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                    </a>
+
+                    <p>
+                    
+                        <a
+                        
+                            target="_blank"
+                            rel="noreferrer"
+                        >
+                        </a>
+                    </p>
+                </div>
+            </footer>
+        </>
+    );
+};
+
+export default Footer;
---
+++ src/data/productsData.js
@@ -0,0 +1,68 @@
+const productsData = [
+    {
+        id: 1,
+        img: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309_FMT_WHH?wid=508&hei=472&fmt=p-jpg&qlt=95&.v=1693086369818",
+        rating: "★★★★",
+        title: "IPhone 15",
+        price: 150000,
+        quantity: 1,
+    },
+    {
+        id: 2,
+        img: "https://images.samsung.com/is/image/samsung/p6pim/in/2302/gallery/in-galaxy-s23-s918-446812-sm-s918bzrcins-534868449?$650_519_PNG$",
+        rating: "★★★★★",
+        title: "Samsung S23 ULTRA",
+        price: 123000,
+        quantity: 1,
+    },
+    {
+        id: 3,
+        img: "https://oasis.opstatics.com/content/dam/oasis/page/xueying/images-top-kv.jpg",
+        rating: "★★★★",
+        title: "OnePlus Fold",
+        price: 134000,
+        quantity: 1,
+    },
+    {
+        id: 4,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShdBH4658d54F18lPhszHgLu2LEvHKcCOJMw&s",
+        rating: "★★★★★",
+        title: "MI 11ULTRA",
+        price: 100000,
+        quantity: 1,
+    },
+    {
+        id: 5,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgZUvgFITxJjKe_iaO0OPSohGJb4-W-n0V1Q&s",
+        rating: "★★★★",
+        title: "Redmi 13C",
+        price: 13000,
+        quantity: 1,
+    },
+    {
+        id: 6,
+        img: "https://fdn2.gsmarena.com/vv/bigpic/xiaomi-mi-11-lite-4g.jpg",
+        rating: "★★★★★",
+        title: "Mi 11lite",
+        price: 31500,
+        quantity: 1,
+    },
+    {
+        id: 7,
+        img: "https://ohlocal-media.s3.amazonaws.com/media/SM109120XI.jpg",
+        rating: "★★★★",
+        title: "Mi 11T PRO",
+        price: 31000,
+        quantity: 1,
+    },
+    {
+        id: 8,
+        img: "https://m.media-amazon.com/images/I/71C8O0yT7hL._AC_UF1000,1000_QL80_.jpg",
+        rating: "★★★★",
+        title: "Redmi Note 13 PRO",
+        price: 24999,
+        quantity: 1,
+    }
+];
+
+export default productsData;
